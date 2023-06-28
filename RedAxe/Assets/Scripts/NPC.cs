b790cb49e966f1ccb0eaefd263c12f6ac666b8ac
Yusuf AĞAÇ <74566648+Yusuf-Agac@users.noreply.github.com>
2023-06-28 15:02:08 +0300

    feat: NPC Name Tag

--- RedAxe/Assets/Scripts/NPC.cs
+++ RedAxe/Assets/Scripts/NPC.cs
@@ -9,19 +9,31 @@ using UnityEngine.UI;
 public class NPC : MonoBehaviour
 {
     public CarAttributes carAttributes;
-    
-    
+
     private FirstPersonMovement playerMovement;
     private PlayerActions playerActions;
+    private RectTransform canvasRectTransform;
 
     private void Awake()
     {
         playerMovement = GameObject.FindWithTag("Movement").GetComponent<FirstPersonMovement>();
         playerActions = playerMovement.transform.parent.GetComponent<PlayerActions>();
+        canvasRectTransform = transform.GetChild(0).GetComponent<RectTransform>();
     }
-
+    
+    private void Update()
+    {
+        CanvasTagLookPlayer();
+    }
+    
     public void StartInteraction()
     {
         playerActions.DisablePlayer(true, carAttributes, gameObject);
+    }
+
+    private void CanvasTagLookPlayer()
+    {
+        canvasRectTransform.LookAt(playerMovement.transform);
+        canvasRectTransform.Rotate(0, 180, 0);
     }
 }
